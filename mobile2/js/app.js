369c1b276fb836248a083f9b5073658f0e6a1962
Gabo Rodriguez <galfo_solano_@hotmail.com>
2016-10-02 20:14:53 -0600

    Add files via upload

--- mobile2/js/app.js
+++ mobile2/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
  	
-    $("#accesorios").on('click', function(){
+    $("#accesorios").on('click touchstart',function(){
 		$('#resultado-accesorios').empty();
 		$.getJSON("http://pymesv.com/cliente02w/API/categorias/?categoria=1")
 		.done(function(datos_del_ws){
@@ -8,13 +8,13 @@ $(document).ready(function(){
 				$("#resultado-accesorios").append("<div id='nombre'><ul><img src=' " + valor.url + " ' height='250' width='250' </ul></div>");
 				$("#resultado-accesorios").append("<div id='imagen'><ul>" + valor.nombre + "</ul></div>");
                 $("#resultado-accesorios").append("<div id='precio'><ul> $" + valor.precio + "</ul></div><br>");
+				 
 			})
 		});
-	})
-    ; 
+	}); 
     
     
-	$("#compus").on('click', function(){
+	$("#compus").on('click touchstart', function(){
 		$('#resultado-compus').empty();
 		$.getJSON("http://pymesv.com/cliente02w/API/categorias/?categoria=2")
 		.done(function(datos_del_ws){
@@ -22,12 +22,11 @@ $(document).ready(function(){
                 $("#resultado-compus").append("<div id='nombre'><ul><img src=' " + valor.url + " ' height='250' width='250' </ul></div>");
 				$("#resultado-compus").append("<div id='imagen'><ul>" + valor.nombre + "</ul></div>");
                 $("#resultado-compus").append("<div id='precio'><ul> $" + valor.precio + "</ul></div><br>");
-                
 			})
 		});
 	});
 
-    $("#memorias").on('click', function(){
+    $("#memorias").on('click touchstart', function(){
 		$('#resultado-usb').empty();
 		$.getJSON("http://pymesv.com/cliente02w/API/categorias/?categoria=3")
 		.done(function(datos_del_ws){
@@ -35,11 +34,12 @@ $(document).ready(function(){
 				$("#resultado-usb").append("<div id='nombre'><ul><img src=' " + valor.url + " ' height='250' width='250' </ul></div>");
 				$("#resultado-usb").append("<div id='imagen'><ul>" + valor.nombre + "</ul></div>");
                 $("#resultado-usb").append("<div id='precio'><ul> $" + valor.precio + "</ul></div><br>");
+				 
 			})
 		});
 	}); 
     
-    $("#programas").on('click', function(){
+    $("#programas").on('click touchstart', function(){
 		$('#resultado-programas').empty();
 		$.getJSON("http://pymesv.com/cliente02w/API/categorias/?categoria=4")
 		.done(function(datos_del_ws){
@@ -47,8 +47,10 @@ $(document).ready(function(){
 				$("#resultado-programas").append("<div id='nombre'><ul><img src=' " + valor.url + " ' height='250' width='250' </ul></div>");
 				$("#resultado-programas").append("<div id='imagen'><ul>" + valor.nombre + "</ul></div>");
                 $("#resultado-programas").append("<div id='precio'><ul> $" + valor.precio + "</ul></div><br>");
+				 
 			})
 		});
 	}); 
 
 });
+
